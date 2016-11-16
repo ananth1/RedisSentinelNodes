c8f44721b3885919b3d55e6cefb85e2a465d6185
Lokhendro <Lokhendro@gmail.com>
2016-11-16 16:47:04 +0530

    Logging
    Logging out nohup

--- startNode.sh
+++ startNode.sh
@@ -7,5 +7,5 @@ fi
 
 nodeName=$1
 
-nohup redis-server /etc/redis/$nodeName/redis.conf </dev/null >/dev/null 2>&1 &
-nohup redis-sentinel /etc/redis/$nodeName/sentinel.conf </dev/null >/dev/null 2>&1 &
+nohup redis-server /etc/redis/$nodeName/redis.conf > $1-node.log 2>&1 &
+nohup redis-sentinel /etc/redis/$nodeName/sentinel.conf > $1-sentinel.log 2>&1 &
